5403529632b5da27e54591eff0bc928d20b3dcfc
shashwatmaru <smaru1@binghamton.edu>
2019-03-31 14:17:51 -0400

    Code to find the longest substring with atmost k distinct character with 2 approaches, First using Hashmap( slower) and other with an int array(better solution).

--- LongestSubstringWithAtMostKDistinctChars.java
+++ LongestSubstringWithAtMostKDistinctChars.java
@@ -1,8 +1,47 @@
 import java.util.Map;
 import java.util.HashMap;
-
+/*
+340. Longest Substring with At Most K Distinct Characters
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+Example 1:
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: T is "ece" which its length is 3.
+Example 2:
+Input: s = "aa", k = 1
+Output: 2
+Explanation: T is "aa" which its length is 2.
+ */
 public class LongestSubstringWithAtMostKDistinctChars {
-        public int lengthOfLongestSubstringKDistinct(String s, int k) {
+    public int lengthOfLongestSubstringKDistinctWithIntArray(String s, int k) {
+        int size = s.length();
+        char[] ch = s.toCharArray();
+        int[] array = new int[256]; //Total 256 ASCII value is there.
+        int i=0;
+        int num=0;
+        int result=0;
+        //ccaabbb
+        for(int j=0;j<size;j++){
+            if(array[ch[j]]==0){
+                num++;
+            }
+            array[ch[j]] += 1;
+
+            while(num>k && i<size){ //sliding window
+                array[ch[i]] -= 1;
+                if(array[ch[i]]== 0){
+                    num--;
+                }
+                i++;
+            }
+
+            result = Math.max(result, j-i+1);
+        }
+        return result;
+
+    }
+
+        public int lengthOfLongestSubstringKDistinctWithHashMap(String s, int k) {
             if(s == null) return 0;
             if(s.length() == 0) return 0;
             int size = s.length();
@@ -11,7 +50,7 @@ public class LongestSubstringWithAtMostKDistinctChars {
             int end =0;
             Map<Character,Integer> map = new HashMap<>();
             char[] ch = s.toCharArray();
-//            System.out.println("MAX, END, STR, MAP");
+         //   System.out.println("MAX, END, STR, MAP");
             while(end < size){ //ccaabbb
                 if(map.containsKey(ch[end])){
                     map.put(ch[end],map.get(ch[end])+1);
@@ -28,7 +67,7 @@ public class LongestSubstringWithAtMostKDistinctChars {
                 }
 
                 maxLen = Math.max(maxLen, end-start+1);
-  //              System.out.println(maxLen+" , "+end+" , "+start+" ,"+map.size());
+           //     System.out.println(maxLen+" , "+end+" , "+start+" ,"+map.size());
                 end++;
 
             }
@@ -39,7 +78,9 @@ public class LongestSubstringWithAtMostKDistinctChars {
         public static void main(String [] args){
             String s = "ccaabbb";
             LongestSubstringWithAtMostKDistinctChars longestSubstringWithAtMostKDistinctChars = new LongestSubstringWithAtMostKDistinctChars();
-            System.out.println(longestSubstringWithAtMostKDistinctChars.lengthOfLongestSubstringKDistinct(s,2));
+            System.out.println("HASHMAP SOLUTION: "+longestSubstringWithAtMostKDistinctChars.lengthOfLongestSubstringKDistinctWithHashMap(s,2));
+            System.out.println("INT ARRAY SOLUTION: "+longestSubstringWithAtMostKDistinctChars.lengthOfLongestSubstringKDistinctWithIntArray(s,2));
+
         }
 
 }
